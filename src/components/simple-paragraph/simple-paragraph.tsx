fadad902908b644c900885106272bb2640e9c0df
freeintelligence <free04381@gmail.com>
2019-02-06 03:25:01 -0300

    Commit to explain a bit about the package: it basically deals with web components to show information in a simple way, currently there are two components (although only one works).

--- src/components/simple-paragraph/simple-paragraph.tsx
+++ src/components/simple-paragraph/simple-paragraph.tsx
@@ -3,7 +3,7 @@ import { Component, Prop } from '@stencil/core'
 @Component({
   tag: 'simple-paragraph',
   styleUrl: 'simple-paragraph.css',
-  shadow: true
+  shadow: false
 })
 export class SimpleParagraph {
 
