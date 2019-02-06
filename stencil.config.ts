9736c3e1d4f012ddf4f23641c986233be07afc19
freeintelligence <free04381@gmail.com>
2019-02-06 11:47:28 -0300

    'www' to 'docs'.

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,6 +8,11 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null // disable service workers
-    }
+    },
+    {
+      type: 'www',
+      dir: 'docs',
+      serviceWorker: null // disable service workers
+    },
   ]
 };
